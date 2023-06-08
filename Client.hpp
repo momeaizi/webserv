859e046e385a0284ee512a390a56f13db71be95e
Abdelmounim Skerba <mskerba@e2r6p9.1337.ma>
2023-06-08 16:37:12 +0100

    creata a Client class

---
+++ Client.hpp
@@ -0,0 +1,15 @@
+#include "Location.hpp"
+
+class Client
+{
+    int                                 clSocker;
+    int                                 pahse;
+    int                                 fd;
+    unsigned int                        serverId;
+    std::string                         URI;
+    std::string                         resources
+    std::map<std::string, std::string>  headerFields;
+    Location                            *location;
+
+
+}
