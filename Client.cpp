24a7c1a6b9378c683851cc6f452bdb543a64f824
Abdelmounim Skerba <mskerba@e2r2p12.1337.ma>
2023-06-09 07:21:49 +0100

    create getline & tok func of string and finish parse func

---
+++ Client.cpp
@@ -0,0 +1,84 @@
+#include "Client.hpp"
+#include <sys/socket.h>
+
+#define MAX 2043
+#define buf 1024
+
+std::vector<std::string> getline(int phase)
+{
+
+    static                    std::string str;
+    char                      buffer[1024];
+    std::vector<std::string>  lines;
+    int                       loc;
+    int                       bytes;
+    int                       fd;
+
+    if (phase) return {str};
+    bytes  = recv(fd, buffer, (size_t)buf);
+    str   += std::string(buffer, bytes);
+    loc  = str.find("\r\n");
+
+    while (loc != -1)
+    {
+            loc = str.find("\r\n");
+            lines.push_back(std::string(str, loc));
+            str.erase(0, loc);
+            // str = std::string(str.ptr + loc + 2, str.size() - loc);
+            if (lines.back() == "")
+                break ;
+    }
+    return lines;
+}
+
+std::string tok(std::string &str, std::string sep)
+{
+    std::string sepstring;
+    int loc = str.find(sep);
+    sepstring = std::string(str, loc);
+    str = str.substr(loc);
+    return sepstring;
+}
+
+void Client::parse()
+{
+    std::string str;
+    std::string buffer;
+    std::vector<std::string> lines;
+    std::vector<std::string>::iterator it;
+
+    lines = getline(0);
+    if (lines.empty())
+        return ;
+    for (it = lines.begin(); it != lines.end(); it++)
+    {
+        str = *it;
+        if (str == "")
+        {
+
+            buffer = getline(1)[0];
+            phase = 0; // request is finished
+            return ;
+        }
+        if (this->methodType == "") 
+        {
+              this->methodType = tok(str, " ");
+              URI = tok(str, " ");
+              if (URI.size() > MAX)
+                  send_414(); // 
+              IsUriValid(URI);
+              if (tok(str, "\r\n") != "HTTP/1.1")
+                  send_505();
+        }
+        else
+        {
+              int index = str.find(":");
+              int len = str.length();
+              std::string   name = str.substr(0, index);
+              if (this->headerFields.count(name))
+                  send_400();
+              std::string   val = str.substr(index, len);
+              headerFields [name] = val;//val.trim(" ");
+        }
+    }
+}
--- Client.hpp
+++ Client.hpp
@@ -1,15 +1,26 @@
+# ifndef CLIENT_HPP
+# define CLIENT_HPP
+
 #include "Location.hpp"
+// #include <vector>
 
 class Client
 {
-    int                                 clSocker;
-    int                                 pahse;
-    int                                 fd;
-    unsigned int                        serverId;
-    std::string                         URI;
-    std::string                         resources
-    std::map<std::string, std::string>  headerFields;
-    Location                            *location;
+    public:
+        int                                 clSocker;
+        int                                 phase;
+        int                                 fd;
+        unsigned int                        serverId;
+        std::string                         methodType;
+        std::string                         URI;
+        std::string                         resources;
+        std::map<std::string, std::string>  headerFields;
+        Location                            *location;
+    
+    // member func
+    void parse();
 
 
 }
+
+#endif
